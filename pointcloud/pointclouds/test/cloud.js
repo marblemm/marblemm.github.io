fbe4b637ad77b8874e47b3f82dae9f33b8256c67
mabo <835621625@qq.com>
2021-08-07 23:03:34 +0800

    Site updated: 2021-08-07 23:01:59

---
+++ pointcloud/pointclouds/test/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=tmerc +lat_0=0 +lon_0=108 +k=1 +x_0=500000 +y_0=0 +ellps=GRS80 +units=m +no_defs",
+    "points": 10000000,
+    "boundingBox": {
+        "lx": 588054.5117549588,
+        "ly": 3781791.7059983166,
+        "lz": 444.1961869112924,
+        "ux": 588860.2377549586,
+        "uy": 3782597.431998316,
+        "uz": 1249.9221869110838
+    },
+    "tightBoundingBox": {
+        "lx": 588054.5107549587,
+        "ly": 3781791.7049983164,
+        "lz": 444.19518691129238,
+        "ux": 588530.9187549588,
+        "uy": 3782597.431998316,
+        "uz": 576.3821869112924
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 6.977791786193848,
+    "scale": 0.01,
+    "hierarchyStepSize": 5
+}
